e8f2c71a580694659775c0d46a84671fdf0e51a5
Rodrigo Engelberg <rodrigoengelberg@gmail.com>
2021-06-10 17:36:59 -0300

    Alteração package-lock.json

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  distDir: "_next",
+  generateBuildId: async () => {
+    if (process.env.BUILD_ID) {
+      return process.env.BUILD_ID;
+    } else {
+      return `${new Date().getTime()}`;
+    }
+  },
   images: {
     domains: ['res.cloudinary.com'],
   },
